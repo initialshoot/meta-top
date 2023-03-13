170846834d98dbc039c5895fc194a86b2ca75b1d
Omar Sanchez <initialshoot@outlook.com>
2023-03-12 18:31:00 -0600

    feat: add the geolocation module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
     { title: 'Home', url: '/folder/Home', icon: 'fa-solid fa-house' },
     { title: 'Hearthstone', url: '/folder/Hearthstone', icon: 'fa-solid fa-hat-wizard' },
     { title: 'MarvelSnap', url: '/folder/MarvelSnap', icon: 'fa-solid fa-cube' },
+    { title: 'Location', url: '/folder/Location', icon: 'fa-solid fa-location-crosshairs' },
   ];
 
   public logAction = [
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { info } from 'console';
+
+declare var google: { maps: { Map: new (arg0: any, arg1: { center: { lat: number; lng: number; }; zoom: number; disableDefaultUI: boolean; }) => any; InfoWindow: new () => any; ControlPosition: { TOP_CENTER: string | number; }; event: { addListenerOnce: (arg0: any, arg1: string, arg2: () => void) => void; }; }; }
 
 @Component({
   selector: 'app-folder',
@@ -7,11 +10,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+
+  public map: any = google.maps.Map;
+
   public folder!: string;
 
   public text = "";
 
   public hsDecks = []
+
+  public onePiece: any;
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
@@ -26,6 +34,57 @@ export class FolderPage implements OnInit {
       } catch (err) {}
     }
   }
+
+  async loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: any = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = {lat: 21.883182, lng: -102.295248};
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 15,
+      disableDefaultUI: true,
+    });
+
+    const infoWindow = new google.maps.InfoWindow();
+
+    const locationButton = document.createElement("button");
+
+    locationButton.textContent = "Get Current Location";
+    locationButton.classList.add("btn");
+    locationButton.classList.add("btn-secondary");
+    locationButton.classList.add("btn-lg");
+    locationButton.classList.add("mt-3");
+
+    this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+    locationButton.addEventListener("click", () => {
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+  
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('<div class="fs-2 fw-bold black btn-dark p-2 rounded"> You Are Here <i class="fa-solid fa-location-dot"></i> </div>');
+            infoWindow.open(this.map);
+            this.map.setCenter(pos);  
+          }
+        );
+      }
+      
+    });
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      /*this.renderMarkers();*/
+      mapEle.classList.add('show-map');
+    });
+  }
+
 
 }
 
